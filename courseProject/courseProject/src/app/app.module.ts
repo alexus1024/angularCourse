6469e5032f13bcf840ea16a4f6af3a00b18614bb
Alexey Romanovsky <alexus1024@gmail.com>
2017-09-15 22:22:44 +0500

    course project components

--- courseProject/courseProject/src/app/app.module.ts
+++ courseProject/courseProject/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from "./RecipeBook/recipe-list/recipe-list.component";
+import { RecipeListItemComponent } from './RecipeBook/recipe-list-item/recipe-list-item.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListItemComponent } from './Shopping/shopping-list-item/shopping-list-item.component';
+import { RecipeDetailComponent } from './RecipeBook/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent, RecipeListItemComponent, RecipeDetailComponent,
+     ShoppingListComponent, ShoppingListItemComponent, HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
