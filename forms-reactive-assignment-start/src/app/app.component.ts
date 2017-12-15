5d59d85226c572b23a9953ca63a1fcdf303148cf
Unknown <rav@tochka.com>
2017-12-15 22:00:30 +0500

    reactive forms assign fin

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Promise } from 'q';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class  AppComponent implements OnInit {
+
+  projectsForm: FormGroup;
+
+ ngOnInit() {
+  this.projectsForm = new FormGroup({
+   'name': new FormControl('al', Validators.required, this.forbiddenName),
+   'mail': new FormControl(null, [Validators.email]),
+   'status': new FormControl('s'),
+
+  });
+ }
+
+ onSubmit() {
+  console.log(this.projectsForm);
+  }
+
+
+
+
+ forbiddenName(control: FormControl): Observable<any> {
+  return  Observable.create(observer => {
+     if ( control.value == 'Test') {       
+      observer.next({'forbiddenName': true});
+     }else {
+       observer.next(null);
+      }
+
+    observer.complete();
+  });
+
+ }
+
+
 }
